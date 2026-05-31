a2fef5254f2643a6230a4920a13c9e8e6b98e026
ChairGoblin <edenop69@gmail.com>
2026-06-01 04:45:36 +0530

    Updated ternary file

---
+++ Learning/12-TernaryOperator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+        //              ternary operator is like boolean operator but used in place of if/else functions.
+
+// combining bool with ternary
+
+bool okay;
+
+cout << "are you okay? Yes(1) or No(0)" << '\n';
+
+cin >> okay;
+
+
+
+
+    okay ? cout << "that's good! hope you have a wonderful day" : cout << "please don't hesitate to contact the national helpline number: xxxxxxxxxxx.";
+
+// if/else would be more appropriate to use with this kind of thing- but i just wanted to test if i can make bool work.
+
+
+
+}
