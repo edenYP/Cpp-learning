51586d9b555221ac29321da6f34d042162a867d5
ChairGoblin <edenop69@gmail.com>
2026-06-01 04:21:13 +0530

    Projects
    Some projects I'm working on or have finished using whatever I've learned so far.
    
    Projects get version updates whenever I add something cool, fix something broken, or accidentally create a new problem :D.

---
+++ Projects/BasicCalculator_v1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+
+
+int main(){
+
+
+    double a;
+    double b;
+    double result;
+    
+    char operation;
+
+
+
+    cout << "*****************EDEN'S CALCULATOR V1*******************" << '\n';
+
+
+    cout << "" << '\n';
+    
+    
+    
+    cout << "What do you want to do? (+ - / * %)" << '\n';
+    
+
+    cin >> operation;
+
+    cout << "enter value for a" << '\n';
+            cin >> a;
+            cout << "enter value for b" << '\n';
+            cin >> b;
+
+
+
+    switch(operation){
+
+        case '+':
+
+
+
+            result = a + b;
+
+            cout << "The sum of those two numbers is " << result << '\n';
+
+        break;
+
+        case '-':
+
+            result = a - b;
+         
+            cout << "From subtracting b from a, we get " << result << '\n';
+
+
+        break;
+
+        case '/':
+
+            result = a/b;
+            cout << "From dividing a from b, we get " << result << '\n';
+
+        break;
+
+        case '*':
+
+            result = a*b;
+
+            cout << "The product of a and b is  " << result << '\n';
+
+
+        break;
+
+
+        case '%':
+
+            result = (int)a % (int) b;
+
+           cout << "the remainder from diving a by b is " << result << '\n';
+           
+        break;
+        
+
+        default: 
+        
+        cout << "Please enter an operator from the operations given above" << '\n';
+
+
+
+    }
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+}
---
+++ Projects/HypotenuseCalculator_v1.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    //          If = used for any condition, else if = used for any other condition, else = used for anything that does not fit the above conditions. they check it from top to bottom.
+
+
+
+
+//                                      HYPOTENUSE CALCULATOR FOR ANY SIDE
+
+
+double a;
+double b;
+double c;
+
+int Calculate;
+
+
+cout << "what do you want to calculate? Hypotenuse (1), Perpendicular (2), Base (3)" << '\n';   // A = base, B = perpendicular, C = hypotenuse
+
+cin >> Calculate;
+
+
+
+
+
+if (Calculate == 1)
+{   
+
+    cout << "Enter the Base" << '\n';
+
+    cin >> a;
+
+    cout << "Enter the Perpendicular";
+ 
+    cin >> b;
+
+    c = sqrt(pow(a,2) + pow(b,2));
+
+
+    cout << "The hypotenuse is " << c << '\n';
+
+
+}
+
+else if (Calculate == 2){
+
+    cout << "enter the hypotenuse" << '\n';
+
+    cin >> c;
+
+    cout << "enter the base" << '\n';
+
+    cin >> a;
+
+    b = sqrt(pow(c,2) - pow(a,2));
+
+    cout << "your perpendicular is " << b << '\n';
+
+
+}
+
+
+
+else if (Calculate == 3){
+
+    cout << "enter your hypotenuse" << '\n';
+
+    cin >> c;
+
+    cout << "enter your base" << '\n';
+
+    cin >> b;
+
+    a = sqrt(pow(c,2) - pow(b,2));
+
+    cout << "your base is " << a << '\n';
+
+}
+
+ else{
+
+    cout << "invalid side you dumbass";
+
+ }
+
+
+
+
+cin.ignore();
+cin.get();
+
+
+return 0;
+
+
+
+
+}
