cd462b4f0ae5354972b43dfdf93f71bac379a7ab
ChairGoblin <edenop69@gmail.com>
2026-06-24 21:42:02 +0530

    Add files via upload

---
+++ Learning/34-MemoryAddresses.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <limits>
+
+
+
+using namespace std;
+
+
+
+//                                                                                  & = USED TO TELL THE MEMORY ADDRESS OF A VARIABLE.
+
+
+
+int main(){
+
+
+
+int age = 29;
+string name = "pokemon";
+bool student = true;
+
+
+cout << &age << '\n';
+cout << &name << '\n';
+cout << &student << '\n';
+
+
+
+//                                      THIS WILL PRINT THE MEMORY ADDRESSES OF THE FUNCTIONS IN HEXADECIMAL. YOU CAN CONVERT THOSE HEXADECIMALS INTO DECIMAL AND CAN USE IT TO FIND THE BYTES EACH VARIABLES TAKES.
+
+
+
+
+
+
+
+
+
+
+
+}
