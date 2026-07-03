88dd1ff913cf417bb6857d53323dd8286592a0c7
ChairGoblin <edenop69@gmail.com>
2026-07-03 23:22:05 +0530

    TicTacToe(V1) Finished
     Implemented player vs computer gameplay
    - Added random computer move generation
    - Added win detection (rows, columns, diagonals)
    - Added draw detection
    - Prevented overwriting occupied squares
    - Added input validation and cin.fail() handling
    - Added board reset for replay
    - Improved game flow and fixed edge cases

--- Projects/TicTacToe.cpp
+++ Projects/TicTacToe.cpp
@@ -8,10 +8,11 @@
 
 
 
-
-void wincondition();
-void userinput(int userresponse, char userside);
-void tictactoe(char userside);
+int wincondition(char userside, char computerside);
+void userinput(char userside, char computerside);
+void tictactoe(char userside, char computerside);
+void compinput(char computerside, char userside);
+void resetboard();
 
 char board[3][3] = {{'1','2','3'},
                      {'4','5','6'},
@@ -21,15 +22,27 @@ char board[3][3] = {{'1','2','3'},
 
 
 int main(){
+    srand(time(0));
     do{
+        bool result;
         char userside;
+        char computerside;
         char response;
-        std::cout << "Do you want to play TicTacToes? (y/n) \n";
+        std::cout << "Do you want continue? (y/n) \n";
         std::cin >> response;
     
-        if(response == 'y'){
+        if(response == 'y' || response == 'Y'){
             std::cout << "Do you want to play as X or O?\n";
             std::cin >> userside;
+
+                if(userside == 'X' || userside == 'x'){
+                        computerside = 'O';
+                }
+                else if(userside == 'O' || userside == 'o'){
+                     computerside = 'X';
+                }
+
+
             
         std::cout << "Aight bet, goodluck! have fun!\n";
 
@@ -37,16 +50,28 @@ int main(){
 
             for(int i = 0; i < 9; i++){
 
-            tictactoe(userside);
-            wincondition();
+            tictactoe(userside, computerside);
+            int result = wincondition(userside, computerside);
+            if(result == 0){ 
+                continue;
+            }
+            if(result == 1){
+                break;
+            }
+            else if(result == 2){
+                                            std::cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << '\n' << "----------"  << '\n' << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << '\n' << "----------" << '\n' << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << "\n";
+                std::cout << "YOU LOST" << '\n';
+                break;
+            }
+
+            if(i == 8){
+                std::cout << "TIE BETWEEN YOU TWO" << '\n';
+            }
             }
 
 
 
         }
-
-
-
 
 
         else if(response == 'n' || response == 'N'){
@@ -55,9 +80,17 @@ int main(){
         }
 
 
+        
+            std::cout << "Do you want to play again? (y/n)\n";
+            std::cin >> response;
 
-
-
+            if(response == 'y' || response == 'Y'){
+                resetboard();
+                continue;
+            }
+            if(response == 'n' || response == 'N'){
+                std::cout << "SEE YOU LATER!\n";
+            }
 
     }while(true);
 
@@ -67,21 +100,12 @@ int main(){
 }
 
 
-void tictactoe(char userside){
-    int userresponse;
+void tictactoe(char userside, char computerside){
 
 
 std::cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << '\n' << "----------"  << '\n' << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << '\n' << "----------" << '\n' << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << "\n";
 
-std::cout << "***************************************\n";
-
-std::cout << "Pick a square to mark as" << userside <<  " -(1,2,3... etc)\n" << '\n';
-
-std::cin >> userresponse;
-
-
-userinput(userresponse, userside);
-
+userinput(userside, computerside);
 
 
 }
@@ -99,7 +123,24 @@ userinput(userresponse, userside);
 
 
 
-void userinput(int userresponse, char userside){
+void userinput(char userside, char computerside){
+int userresponse;
+do{
+
+std::cout << "***************************************\n";
+
+std::cout << "Pick a square to mark as" << userside <<  " -(1,2,3... etc)\n" << '\n';
+
+std::cin >> userresponse;
+
+    if(std::cin.fail()){
+         std::cin.clear();
+         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+         std::cout << "Invalid input son.";
+         continue;
+    }
+
+
 
 /*
 
@@ -187,11 +228,98 @@ so we'll get something like- 1 % 3 = 1,    2 % 3 = 2,    3 % 3 = 0...
 
 
 */
+  
+if( 0 < userresponse && userresponse <= 9){
 
-int row = (userresponse - 1)/3;
-int col = (userresponse - 1) % 3;
+        int row = (userresponse - 1)/3;
+        int col = (userresponse - 1) % 3;
 
-board[row][col] = userside;
+            if(board[row][col] == userside || board[row][col] == computerside){
+                std::cout << "ALREADY TAKEN SPOT, DON'T CHEAT SON\n";
+                continue;
+            }
+
+            else{
+            board[row][col] = userside;
+            int win = wincondition(userside, computerside);
+            if(win == 1){
+                std::cout << "YOU WIN!" << '\n';
+                break;
+            }
+
+            compinput(computerside, userside);
+            break;
+            }
+        }
+
+else{
+    std::cout << "INVALID SPOT SON, you stupid or wut?\n";
+   continue;
+}
+
+
+}while(true);
+
+
+
+
+
+
+
+}
+
+void compinput(char computerside, char userside){
+bool available = false;
+
+for(int i = 0; i < 3 && !available; i++){
+    for(int j = 0; j < 3; j++){
+        if(board[i][j] == userside || board[i][j] == computerside){
+            continue;
+        }
+        else if(board[i][j] != userside && board[i][j] != computerside){
+             available = true;   
+            break;
+        }
+    }
+}
+
+
+
+
+if(available == true){
+
+
+    do{
+                int compresponse = rand() % 10;
+                
+                    if(compresponse == 0){
+                        compresponse++;
+                    }
+
+                int row = (compresponse - 1)/3;
+                int col = (compresponse - 1) % 3;
+
+                    
+
+
+
+                if(board[row][col] == userside || board[row][col] == computerside){
+                    continue;
+                }
+
+                else{
+                    board[row][col] = computerside;
+                    break;
+                }
+
+
+
+
+    }while(true);
+
+
+}
+
 
 
 }
@@ -199,9 +327,156 @@ board[row][col] = userside;
 
 
 
-void wincondition(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int wincondition(char userside, char computerside){
+
+
+
+
+// CHECKING USER SIDE:
+
+
+// checking the rows first.
+
+for(int i = 0; i < 3; i++){
+
+ if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] == userside){
+    return 1;
+ }
+}
+
+// checking the coloumns.
+
+for(int j = 0; j < 3; j++){
+
+    if(board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[2][j] == userside){
+        return 1;
+    }
+    
+}
+
+// Checking the two diagonals.
+
+
+if(
+board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == userside ||
+board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == userside)
+{
+    return 1;
+}
+
+
+// CHECKING COMPUTER SIDE
+
+
+
+// checking the rows first.
+
+for(int i = 0; i < 3; i++){
+
+ if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] == computerside){
+    return 2;
+ }
+}
+
+// checking the coloumns.
+
+for(int j = 0; j < 3; j++){
+
+    if(board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[2][j] == computerside){
+        return 2;
+    }
+    
+}
+
+// Checking the two diagonals.
+
+
+if(
+board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == computerside ||
+board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == computerside)
+{
+    return 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
 
 
 }
+
+
+
+
+
+
+
+
+
+void resetboard(){
+
+
+    for(int i = 1; i < 10; i++){
+
+        int row = (i - 1) / 3;
+        int col = (i - 1) % 3;
+
+        board[row][col] = i + '0';
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
 
 
